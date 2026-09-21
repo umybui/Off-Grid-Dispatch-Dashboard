356f99c7c8591c0e0b572ba91866c39219fb864f
umybui <dennisumlas@gmail.com>
2026-09-22 06:56:10 +0800

    Update mindoro.py

--- config/mindoro.py
+++ config/mindoro.py
@@ -1,11 +1,7 @@
 CONFIG = {
+    "TITLE": "Mainland Mindoro Dispatch Dashboard",
+    "SOURCE_FILE": "Mindoro Dispatch Summary Final.xlsm",
+    "SOURCE_SHEET": "Main Query",
 
-    "TITLE":
-        "Mainland Mindoro Dispatch Dashboard",
-
-    "SOURCE_FILE":
-        "Mindoro Dispatch Summary Final.xlsx",
-
-    "SOURCE_SHEET":
-        "<Main Query>"
+    "SYSTEM_TYPE": "MINDORO"
 }
