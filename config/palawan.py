9294ce5ef93c7d9e828c4067d1a2083a383d0451
umybui <dennisumlas@gmail.com>
2026-09-22 06:55:23 +0800

    Update palawan.py

--- config/palawan.py
+++ config/palawan.py
@@ -1,11 +1,7 @@
 CONFIG = {
+    "TITLE": "Mainland Palawan Dispatch Dashboard",
+    "SOURCE_FILE": "PALECO Dispatch Summary.xlsx",
+    "SOURCE_SHEET": "2  PALECO Reports",
 
-    "TITLE":
-        "Mainland Palawan Dispatch Dashboard",
-
-    "SOURCE_FILE":
-        "PALECO Dispatch Summary.xlsx",
-
-    "SOURCE_SHEET":
-        "2  PALECO Reports"
+    "SYSTEM_TYPE": "PALAWAN"
 }
