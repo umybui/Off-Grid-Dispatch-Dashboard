8ef65cf2a6cff27260b76d0b2124cebadf539435
umybui <dennisumlas@gmail.com>
2026-09-22 11:28:50 +0800

    Update load_data.py

--- core/load_data.py
+++ core/load_data.py
@@ -2,9 +2,12 @@ import pandas as pd
 
 def load_data(config):
 
+    print("CONFIG RECEIVED:", config)
+
     df = pd.read_excel(
         config["FILE_PATH"],
-        sheet_name=config["SHEET_NAME"]
+        sheet_name=config["SHEET_NAME"],
+        engine="openpyxl"
     )
 
     return df
