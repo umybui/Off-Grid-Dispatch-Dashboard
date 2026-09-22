7068cddd345e57b5f2506a682a7d5e0c7c19e61e
umybui <dennisumlas@gmail.com>
2026-09-22 11:31:54 +0800

    Update load_data.py

--- core/load_data.py
+++ core/load_data.py
@@ -1,11 +1,21 @@
 import pandas as pd
+import streamlit as st
+from pathlib import Path
 
 def load_data(config):
 
-    print("CONFIG RECEIVED:", config)
+    file_path = config["FILE_PATH"]
+
+    st.write("File exists:", Path(file_path).exists())
+
+    with open(file_path, "rb") as f:
+        first_bytes = f.read(200)
+
+    st.write("First bytes of file:")
+    st.code(first_bytes)
 
     df = pd.read_excel(
-        config["FILE_PATH"],
+        file_path,
         sheet_name=config["SHEET_NAME"],
         engine="openpyxl"
     )
