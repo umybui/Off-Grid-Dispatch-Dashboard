d6d4f630f43ae40ee50fbdee99194a7962763e26
umybui <dennisumlas@gmail.com>
2026-09-22 12:18:53 +0800

    Update palawan.py

--- config/palawan.py
+++ config/palawan.py
@@ -1,5 +1,12 @@
 CONFIG = {
+
     "SYSTEM_NAME": "Palawan",
+
     "FILE_PATH": "data/PALECO Dispatch Summary.xlsx",
-    "SHEET_NAME": "2  PALECO Reports"
+
+    "SHEET_NAME": "2  PALECO Reports",
+
+    "DATETIME_COLUMN": "Datetime",
+
+    "VALUE_COLUMN": "NumericValue"
 }
