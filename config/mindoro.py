bfc0bc80958d6cf3dbc77a22e0ab062e4f4b9fa2
umybui <dennisumlas@gmail.com>
2026-09-22 12:19:13 +0800

    Update mindoro.py

--- config/mindoro.py
+++ config/mindoro.py
@@ -1,5 +1,12 @@
 CONFIG = {
+
     "SYSTEM_NAME": "Mindoro",
+
     "FILE_PATH": "data/Mindoro Dispatch Summary Final.xlsm",
-    "SHEET_NAME": "Main Query"
+
+    "SHEET_NAME": "Main Query",
+
+    "DATETIME_COLUMN": "Datetime",
+
+    "VALUE_COLUMN": "Value"
 }
