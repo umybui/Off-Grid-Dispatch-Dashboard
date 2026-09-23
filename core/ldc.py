d134f107ac4b584332b2705a2af14fd94b3bad2a
umybui <dennisumlas@gmail.com>
2026-09-23 23:22:21 +0800

    Update ldc.py

--- core/ldc.py
+++ core/ldc.py
@@ -1,1 +1,36 @@
+import pandas as pd
 
+
+def build_ldc(
+    total_demand
+):
+
+    ldc = (
+        total_demand[["Value"]]
+        .rename(
+            columns={
+                "Value": "DemandMW"
+            }
+        )
+        .copy()
+    )
+
+    ldc = ldc.sort_values(
+        "DemandMW",
+        ascending=False
+    ).reset_index(
+        drop=True
+    )
+
+    ldc["HourRank"] = (
+        ldc.index + 1
+    )
+
+    ldc["Percentile"] = (
+        ldc["HourRank"]
+        /
+        len(ldc)
+        * 100
+    )
+
+    return ldc
