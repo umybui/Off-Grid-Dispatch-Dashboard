6b60e4ffcdd652b63bb1fa35d82ddb410220be1c
umybui <dennisumlas@gmail.com>
2026-09-23 09:25:29 +0800

    Update data_prep.py

--- core/data_prep.py
+++ core/data_prep.py
@@ -18,6 +18,16 @@ def prepare_data(df, config):
         errors="coerce"
     )
 
+    scale = config.get("UNIT_SCALE", 1)
+
+    df["Value"] = (
+        pd.to_numeric(
+            df[value_column],
+            errors="coerce"
+        )
+        / scale
+    )
+
     df["Month"] = df["Datetime"].dt.month
 
     df["Day"] = df["Datetime"].dt.day
