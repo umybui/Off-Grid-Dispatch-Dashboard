cf25e708e20287fcd313e4849b9e9fbb5b0c97d7
umybui <dennisumlas@gmail.com>
2026-09-24 11:32:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ try:
     with col4:
         st.metric(
             "Total SSE",
-            f"{ldc_segments['total_sse'\]:,.0f}"
+            f"{ldc_segments['total_sse']:,.0f}"
         )
     
     st.dataframe(
