f96a994314725346efdfede201fc9a930d36273d
umybui <dennisumlas@gmail.com>
2026-09-26 10:20:10 +0800

    Create capacity_reference.py

---
+++ core/capacity_reference.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+
+def build_capacity_reference(
+    df
+):
+
+    if "Attribute" not in df.columns:
+        return pd.DataFrame()
+
+    capacity_data = (
+        df[
+            df["Attribute"]
+            .astype(str)
+            .str.upper()
+            .isin(
+                [
+                    "INSTALLED CAPACITY (MW)",
+                    "DEPENDABLE CAPACITY"
+                ]
+            )
+        ]
+        .copy()
+    )
+
+    if len(capacity_data) == 0:
+        return pd.DataFrame()
+
+    available_capacity_tbl = (
+        capacity_data
+        .pivot_table(
+            index=["Plant"],
+            columns="Attribute",
+            values="Value",
+            aggfunc="max"
+        )
+        .reset_index()
+    )
+
+    available_capacity_tbl.rename(
+        columns={
+            "DEPENDABLE CAPACITY":
+                "DependableMW",
+
+            "INSTALLED CAPACITY (MW)":
+                "InstalledMW"
+        },
+        inplace=True
+    )
+
+    if "DependableMW" in available_capacity_tbl.columns:
+
+        available_capacity_tbl[
+            "AvailableMW"
+        ] = (
+            available_capacity_tbl[
+                "DependableMW"
+            ]
+        )
+
+    return available_capacity_tbl
